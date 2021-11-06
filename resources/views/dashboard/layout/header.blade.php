e1c918ac6fccb17d7962dc4e7168e549b1482c0a
karim mamdouh <mamdouhk03@gmail.com>
2021-11-06 22:34:13 +0200

    video_url

--- resources/views/dashboard/layout/header.blade.php
+++ resources/views/dashboard/layout/header.blade.php
@@ -5,14 +5,8 @@
       <ul class="nav navbar-nav d-xl-none">
         <li class="nav-item"><a class="nav-link menu-toggle" href="javascript:void(0);"><i class="ficon" data-feather="menu"></i></a></li>
       </ul>
-      <ul class="nav navbar-nav bookmark-icons">
-        <li class="nav-item d-none d-lg-block"><a class="nav-link" href="app-email.html" data-toggle="tooltip" data-placement="top" title="Email"><i class="ficon" data-feather="mail"></i></a></li>
-        <li class="nav-item d-none d-lg-block"><a class="nav-link" href="app-chat.html" data-toggle="tooltip" data-placement="top" title="Chat"><i class="ficon" data-feather="message-square"></i></a></li>
-        <li class="nav-item d-none d-lg-block"><a class="nav-link" href="app-calendar.html" data-toggle="tooltip" data-placement="top" title="Calendar"><i class="ficon" data-feather="calendar"></i></a></li>
-        <li class="nav-item d-none d-lg-block"><a class="nav-link" href="app-todo.html" data-toggle="tooltip" data-placement="top" title="Todo"><i class="ficon" data-feather="check-square"></i></a></li>
-      </ul>
+
       <ul class="nav navbar-nav">
-        <li class="nav-item d-none d-lg-block"><a class="nav-link bookmark-star"><i class="ficon text-warning" data-feather="star"></i></a>
           <div class="bookmark-input search-input">
             <div class="bookmark-input-icon"><i data-feather="search"></i></div>
             <input class="form-control input" type="text" placeholder="Bookmark" tabindex="0" data-search="search">
@@ -36,8 +30,6 @@
 
             @endforeach
         </li>
-      <li class="nav-item d-none d-lg-block"><a class="nav-link nav-link-style"><i class="ficon" data-feather="sun"></i></a></li>
-      <li class="nav-item nav-search"><a class="nav-link nav-link-search"><i class="ficon" data-feather="search"></i></a>
           <form method="get" action="{!! LaravelLocalization::localizeUrl('dashboard/search') !!}">
           <div class="search-input">
               <div class="search-input-icon"><i data-feather="search"></i></div>
