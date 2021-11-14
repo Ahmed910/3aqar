f6b9b8065b4e89e57780873df3d30247acc452c7
Ahmed910 <ahmedabotaleb256@gmail.com>
2021-11-14 15:59:37 +0200

    initial commit

--- app/Http/Resources/Api/Owner/Ads/AdResource.php
+++ app/Http/Resources/Api/Owner/Ads/AdResource.php
@@ -39,9 +39,10 @@ class AdResource extends JsonResource
             'ad_rent'=> $this->when($this->ad_type=='rent',new AdRentResource($this->rent)),
             'desc'=>$this->desc,
             'status'=>$this->status,
+            'price_setting' => $this->ad_type == 'sale' ? (int)setting('sale_price') : (int)setting('sale_price'),
             'last_updated_at'=>now()->diffInDays($this->last_updated_at),
             'advertiser'=>new UserDataResource($this->advertiser),
-           
+
              'city'=>new DistrictResource($this->district),
             'watching_number'=>$this->watching_number
         ];
