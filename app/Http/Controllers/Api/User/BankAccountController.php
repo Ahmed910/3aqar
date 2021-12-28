fcdb746ed0cd9234214b67453fd1094a89e69877
Ahmed910 <ahmedabotaleb256@gmail.com>
2021-12-28 11:23:41 +0200

    added sale and rent pric

--- app/Http/Controllers/Api/User/BankAccountController.php
+++ app/Http/Controllers/Api/User/BankAccountController.php
@@ -12,7 +12,7 @@ class BankAccountController extends Controller
     public function index()
     {
         $bank_accounts = BankAccount::paginate(100);
-        return BankAccountResource::collection($bank_accounts)->additional(['status'=>'success','message'=>'']);
+        return BankAccountResource::collection($bank_accounts)->additional(['status'=>'success','rent_price'=>(double)setting('rent_price'),'sale_price'=>(double)setting('sale_price'),'message'=>'']);
     }
 
 
