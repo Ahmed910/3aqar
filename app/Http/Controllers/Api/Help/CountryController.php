02734bc36e2882e361b44d83430c92c27c3e7a47
Ahmed910 <ahmedabotaleb256@gmail.com>
2021-12-28 11:28:50 +0200

    edit district serv

--- app/Http/Controllers/Api/Help/CountryController.php
+++ app/Http/Controllers/Api/Help/CountryController.php
@@ -4,8 +4,8 @@ namespace App\Http\Controllers\Api\Help;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use App\Http\Resources\Api\Help\{CountryResource , CityResource, DistrictResource};
-use App\Models\{Country , City, District};
+use App\Http\Resources\Api\Help\{CountryResource, CityResource, DistrictResource};
+use App\Models\{Country, City, District};
 
 class CountryController extends Controller
 {
@@ -17,27 +17,18 @@ class CountryController extends Controller
     public function index()
     {
         $countries = Country::latest()->get();
-        return CountryResource::collection($countries)->additional(['status' => 'success','message'=>'']);
+        return CountryResource::collection($countries)->additional(['status' => 'success', 'message' => '']);
     }
 
     public function show()
     {
-        $country = Country::first();
-       
-        if(isset($country) && $country)
-        {
-             $districts = District::where('country_id',$country->id)->latest()->get();
-             return DistrictResource::collection($districts)->additional(['status' => 'success','message'=>'']);
-        }
-        return response()->json(['data'=>null,'status'=>'fail','message'=>trans('api.messages.countries_not_found')],400);
-        
-       
+        $districts = District::latest()->get();
+        return DistrictResource::collection($districts)->additional(['status' => 'success', 'message' => '']);
     }
 
     public function getCities()
     {
         $cities = City::latest()->get();
-        return CityResource::collection($cities)->additional(['status' => 'success','message'=>'']);
+        return CityResource::collection($cities)->additional(['status' => 'success', 'message' => '']);
     }
-
 }
