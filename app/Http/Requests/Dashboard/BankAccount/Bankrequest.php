2426372fc684921f642f6d7832971d060dd49a80
Ahmed910 <ahmedabotaleb256@gmail.com>
2022-01-03 11:19:20 +0200

    edit bank account request

--- app/Http/Requests/Dashboard/BankAccount/Bankrequest.php
+++ app/Http/Requests/Dashboard/BankAccount/Bankrequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests\Dashboard\BankAccount;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class Bankrequest extends FormRequest
+class BankRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
