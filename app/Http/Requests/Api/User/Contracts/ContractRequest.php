29b2a4928a34ceff168bd8c632771fb20499e1f0
Ahmed910 <ahmedabotaleb256@gmail.com>
2022-01-03 15:52:06 +0200

    initial commit

--- app/Http/Requests/Api/User/Contracts/ContractRequest.php
+++ app/Http/Requests/Api/User/Contracts/ContractRequest.php
@@ -27,15 +27,31 @@ class ContractRequest extends ApiMasterRequest
         return [
             'identity_number_image_for_owner'=>'required|image|mimes:png,jpg,jpeg,gif',
             'property_document'=>'required|image|mimes:png,jpg,jpeg,gif',
-            'address_image'=>'required|image|mimes:png,jpg,jpeg,gif',
+            'address_image'=>'nullable|image|mimes:png,jpg,jpeg,gif',
             'identity_number_image_for_citizen'=>'required|image|mimes:png,jpg,jpeg,gif',
-            'national_address_for_citizen'=>'required|image|mimes:png,jpg,jpeg,gif',
-            'building_date'=>'required|date_format:Y-m-d',
-            'aqar_number'=>'required|integer',
+            'national_address_for_citizen'=>'nullable|image|mimes:png,jpg,jpeg,gif',
+            'building_date'=>'required|date',
+            'aqar_number'=>'nullable|integer',
             'aqar_address'=>'required|string|between:2,200',
-            'iban_num'=>'required',
-            'phone_for_owner'=>'required|numeric|digits_between:5,20|starts_with:9665,05|unique:contracts,phone_for_owner',
+            'iban_num'=>'nullable',
+           'phone_for_owner'=>'required|numeric|digits_between:5,20|starts_with:9665,05|unique:contracts,phone_for_owner',
             'phone_for_citizen'=>'required|numeric|digits_between:5,20|starts_with:9665,05|unique:contracts,phone_for_citizen',
         ];
     }
+
+    public function getValidatorInstance()
+    {
+       $data = $this->all();
+
+       if (isset($data['building_date']) && $data['building_date'] != null) {
+           $data['building_date'] = date('Y-m-d', strtotime($data['building_date']));
+       }
+
+
+
+
+       $this->getInputSource()->replace($data);
+       return parent::getValidatorInstance();
+    }
+
 }
